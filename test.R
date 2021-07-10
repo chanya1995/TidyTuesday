189e031dc162019f794c770c78558fa2ca79e3ad
chanya1995 <chanyaaaaaaaaa@gmail.com>
2021-07-10 19:33:40 +0700

    second commit

--- test.R
+++ test.R
@@ -4,3 +4,10 @@
 ## Tools -> Version Control -> Select all to commit -> push 
 # To pull files from github
 ## Tools -> VC -> pull branches 
+
+# If any problem with git path 
+## open terminal and type 'which git' 
+## and open global options to make sure that the git locates in the correct location
+
+# If any problem with keychain
+## the password is the same as my laptop log-in password
