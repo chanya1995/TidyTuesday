0f453718572f860f391273b32d8dc8ef07afb35f
chanya1995 <chanyaaaaaaaaa@gmail.com>
2021-07-10 18:40:13 +0700

    first commit

---
+++ test.R
@@ -0,0 +1,6 @@
+## this is for test 
+## thank you
+# To push files to github
+## Tools -> Version Control -> Select all to commit -> push 
+# To pull files from github
+## Tools -> VC -> pull branches 
